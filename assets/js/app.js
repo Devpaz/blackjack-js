ff6e971fd5eb6b36bff55165c67c500fbd36a51e
Paolo Paz Ramos <sl.paolo.paz@gmail.com>
2022-01-09 08:20:40 -0500

    Valor de la Carta

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,7 +41,18 @@ const pedirCarta = () => {
     console.log({carta});
     return carta;
 }
-for (let i=0 ;i<= 100; i++){
-    
-    pedirCarta();
+
+//pedirCarta();
+
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN(valor)  ) ? 
+           ( valor === 'A' ) ? 11 : 10
+           : valor * 1;
+
+
+
 }
+const valor =valorCarta('AD');
+console.log({valor})
